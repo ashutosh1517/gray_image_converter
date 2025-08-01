40df32730fe0a96f2f1ca470ab5729255833f81b
Ashutosh Pal <40272206+ashutosh1517@users.noreply.github.com>
2025-08-01 20:05:25 +0530

    app.py

--- app.py
+++ app.py
@@ -4,17 +4,17 @@ import io
 
 st.title("Image to Grayscale Converter")
 
-# File uploader
-uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+# upload the file
+uploaded_file = st.file_uploader("Upload an image (File Type: JPG, PNG, JPEG)", type=["jpg", "jpeg", "png"])
 
 if uploaded_file:
-    # Open the uploaded image
+    # OPening the image
     original_image = Image.open(uploaded_file)
 
-    # Convert to grayscale
+    # converting to grayscale
     gray_image = original_image.convert("L")
 
-    # Display original and grayscale images side-by-side
+    # showing the original and the grayscale images side-by-side
     col1, col2 = st.columns(2)
     with col1:
         st.header("Original Image")
@@ -23,7 +23,7 @@ if uploaded_file:
         st.header("Grayscale Image")
         st.image(gray_image, use_column_width=True)
 
-    # Provide a download button for grayscale image
+    # download button for grayscale image
     buffer = io.BytesIO()
     gray_image.save(buffer, format="PNG")
     st.download_button(
